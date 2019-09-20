c92eebe79d86cb61f677f8489d5c7805b1737f33
ajiputra19 <ajiputrawicaksanaa19@gmail.com>
2019-09-20 16:27:26 +0700

    Perubahan Intent 1

--- Intent/app/src/main/java/id/ac/polinema/intent/ExplicitIntentActivity.java
+++ Intent/app/src/main/java/id/ac/polinema/intent/ExplicitIntentActivity.java
@@ -9,10 +9,25 @@ import android.widget.TextView;
 
 public class ExplicitIntentActivity extends AppCompatActivity {
 
+    // Mendeklarasikan atribut dengan nama nameInput dengan tipe EditText
+    private EditText nameInput;
+    // Mendeklarasikan atribut dengan nama outputText dengan tipe TextView
+    private TextView outputText;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_explicit_intent);
+
+
+    }
+
+    public void handleExplicitIntent(View view) {
+        nameInput = findViewById(R.id.input_name);
+        outputText = findViewById(R.id.text_output);
+
+        String name = nameInput.getText().toString();
+        outputText.setText("Hello " + name);
     }
 
 }
